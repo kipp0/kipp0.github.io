4f5ebbd99c8e531872018ef08caac2bb05c02046
Pierre Smith <smith.pierre@me.com>
2019-01-21 22:36:01 -0500

    adding react

---
+++ build/precache-manifest.65aebbfcf6cbb83eec09356235340b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f29e2bec53843ec769d9",
+    "url": "/static/css/main.c22cb610.chunk.css"
+  },
+  {
+    "revision": "f29e2bec53843ec769d9",
+    "url": "/static/js/main.f29e2bec.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f6d754a03f91d3f82702fde03c1d172",
+    "url": "/index.html"
+  }
+];
