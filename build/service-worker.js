291210cd0f77a9c8e3aee9ec8c9d4d62c04c2dc5
Pierre Smith <smith.pierre@me.com>
2019-01-21 22:45:26 -0500

    adding react

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65aebbfcf6cbb83eec09356235340b09.js"
+  "/kipp0/kipp0.github.io/precache-manifest.0d6bbc52922671c4c2991d82bd234958.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kipp0/kipp0.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
